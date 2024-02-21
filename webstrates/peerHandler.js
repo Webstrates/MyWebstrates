12a60842122c2d54b67aa93e39dbfb39d9e3f2b9
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2024-02-21 12:41:57 +0100

    Fixed issue with peer list

--- webstrates/peerHandler.js
+++ webstrates/peerHandler.js
@@ -8,12 +8,14 @@ let peers = [];
 coreEvents.addEventListener('message', (message) => {
 	if (message.wa !== 'ping') return;
 	for (let peer of peers) {
-		if (peer.peerId === message.body) return;
+		if (peer.peerId === message.body) {
+			peer.timestamp = Date.now();
+			return;
+		}
 	}
-	if (peers.includes(message.body)) return;
 	peers.push({peerId: message.body, timestamp: Date.now()});
+	coreEvents.triggerEvent('peerListReceived', peers.map(p => p.peerId));
 	coreEvents.triggerEvent('peerConnected', message.body);
-	coreEvents.triggerEvent('peerListReceived', structuredClone(peers));
 });
 
 function ping() {
@@ -21,8 +23,8 @@ function ping() {
 	for (let peer of peers) {
 		if (Date.now() - peer.timestamp > 10000) {
 			peers = peers.filter(p => p !== peer);
+			coreEvents.triggerEvent('peerListReceived', peers.map(p => p.peerId));
 			coreEvents.triggerEvent('peerDisconnected', peer.peerId);
-			coreEvents.triggerEvent('peerListReceived', structuredClone(peers));
 		}
 	}
 }
