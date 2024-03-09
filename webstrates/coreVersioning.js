477856235f8a4d5fafc5f0017cf26e4a38831013
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2024-03-09 20:12:16 +0100

    Added documentation to version module.

--- webstrates/coreVersioning.js
+++ webstrates/coreVersioning.js
@@ -4,19 +4,30 @@ import { globalObject } from './globalObject.js';
 
 const versioningModule = {};
 
+/**
+ * Get the current version number of the strate.
+ */
 Object.defineProperty(globalObject.publicObject, 'version', {
 	get: () => Automerge.getAllChanges(amDoc).length-1,
 });
 
+/**
+ * Get the version hash of the current version.
+ */
 Object.defineProperty(globalObject.publicObject, 'versionHash', {
 	get: () => Automerge.getHeads(amDoc)[0],
 });
 
+/**
+ * Get the list of patches from the current version to the previous given version.
+ * @param handle
+ * @param version
+ * @returns {Promise<Patch[]>}
+ */
 versioningModule.diffFromNewToOld = async (handle, version) => {
 	let currentDoc = await handle.doc();
 	let allChanges = A.getAllChanges(currentDoc);
 	let oldHead;
-	console.log("VERSION", version);
 	if (typeof version === 'number') {
 		const versionIndex = version;
 		if (version >= allChanges.length || version < 1) {
@@ -26,9 +37,7 @@ versioningModule.diffFromNewToOld = async (handle, version) => {
 	} else if (typeof version === 'string') {
 		oldHead = version;
 	}
-	console.log("OLD HEAD", oldHead);
 	const diffFromNewToOld = A.diff(currentDoc, A.getHeads(currentDoc), [oldHead]);
-	console.log("diffFromNewToOld", diffFromNewToOld);
 	return diffFromNewToOld;
 }
 
@@ -41,15 +50,26 @@ versioningModule.restore = async (handle, version) => {
 	}
 }
 
+/**
+ * Restore the strate to a previous version.
+ */
 Object.defineProperty(globalObject.publicObject, 'restore', {
 	value: async (version) => {
 		await versioningModule.restore(handle, version);
 	}
 });
 
+/**
+ * Copy the current strate.
+ * @param handle
+ * @param repo
+ * @param options
+ * @returns {Promise<*>}
+ */
 versioningModule.copy = async (handle, repo, options = {local: false, version: undefined}) => {
 	let sourceDoc;
 	if (options.version) {
+		// We have to create a new source doc from the previous version
 		const diffFromNewToOld = await versioningModule.diffFromNewToOld(handle, options.version);
 		const currentDoc = await handle.doc();
 		sourceDoc = A.clone(currentDoc);
@@ -63,6 +83,7 @@ versioningModule.copy = async (handle, repo, options = {local: false, version: u
 	}
 	let newDocHandle = repo.create();
 	await newDocHandle.change(doc => {
+		// Copy over all properties from the source doc
 		for (const key in sourceDoc) {
 			doc[key] = structuredClone(sourceDoc[key]);
 		}
@@ -73,6 +94,12 @@ versioningModule.copy = async (handle, repo, options = {local: false, version: u
 	return newDocHandle;
 }
 
+/**
+ * Copy the current strate.
+ * options.local: If true, the new strate will not be federated.
+ * options.version: The version to copy from. If undefined, the current version is copied.
+ * The version can either be a number or a version hash.
+ */
 Object.defineProperty(globalObject.publicObject, 'copy', {
 		value: async (options = {local: false, version: undefined}) => {
 			let newDocHandle = await versioningModule.copy(handle, repo, options);
@@ -82,11 +109,21 @@ Object.defineProperty(globalObject.publicObject, 'copy', {
 		}
 });
 
+/**
+ * Clone the current strate.
+ * @param handle
+ * @param repo
+ * @returns {Promise<*>}
+ */
 versioningModule.clone = async (handle, repo) => {
 	let clonedDocHandle = repo.clone(handle);
 	return clonedDocHandle;
 }
 
+/**
+ * Clone the current strate.
+ * Opens the cloned strate in a new tab
+ */
 Object.defineProperty(globalObject.publicObject, 'clone', {
 	value: async () => {
 		let clonedDocHandle = await versioningModule.clone(handle, repo);
@@ -96,6 +133,10 @@ Object.defineProperty(globalObject.publicObject, 'clone', {
 	}
 });
 
+/**
+ * Merge the changes from another strate into the current strate.
+ * @param {string} otherStrateId The id of the strate to merge changes from.
+ */
 Object.defineProperty(globalObject.publicObject, 'merge', {
 	value: async (otherStrateId) => {
 		let otherStrateHandle = repo.find(`automerge:${otherStrateId}`);
