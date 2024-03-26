61c1d57f477374f6c302829722da04495b17f5f8
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2024-03-26 22:41:52 +0100

    Implemented chunking in WebRTC network adapter

--- p2p/WebRTCNetworkAdapter.js
+++ p2p/WebRTCNetworkAdapter.js
@@ -8,6 +8,7 @@ export class WebRTCNetworkAdapter extends NetworkAdapter {
 		this.connection = connection;
 		this.channel = channel;
 		this.options = options;
+		this.chunkMap = new Map();
 		setInterval(() => {
 			this.send({'type': 'ping'});
 		}, 1000);
@@ -19,7 +20,6 @@ export class WebRTCNetworkAdapter extends NetworkAdapter {
 		this.peerMetadata = peerMetaData;
 		this.channel.onmessage = (e) => {
 			const message = decode(new Uint8Array(e.data));
-			console.log("Got message", message);
 
 			if ("targetId" in message && message.targetId !== this.peerId) {
 				return
@@ -48,7 +48,26 @@ export class WebRTCNetworkAdapter extends NetworkAdapter {
 					if (!("data" in message)) {
 						this.emit("message", message)
 					} else {
-						const data = message.data
+						let data = message.data
+						if (message.chunked) {
+							if (!this.chunkMap.has(message.chunked.id)) {
+								this.chunkMap.set(message.chunked.id, {chunks: new Array(message.chunked.total), received: 1})
+							} else {
+								this.chunkMap.get(message.chunked.id).received++;
+							}
+							this.chunkMap.get(message.chunked.id).chunks[message.chunked.index] = data;
+							if (this.chunkMap.get(message.chunked.id).received === message.chunked.total) {
+								let consolidatedData = [];
+								for (let chunk of this.chunkMap.get(message.chunked.id).chunks) {
+									chunk.forEach(c => consolidatedData.push(c));
+								}
+								data = consolidatedData;
+								this.chunkMap.delete(message.chunked.id);
+								delete message.chunked;
+							} else {
+								return;
+							}
+						}
 						this.emit("message", {
 							...message,
 							data: new Uint8Array(data),
@@ -77,21 +96,62 @@ export class WebRTCNetworkAdapter extends NetworkAdapter {
 	}
 
 	send(message) {
-		let payload;
+		let payloads = [];
 		if ("data" in message) {
-			payload = {
-				...message,
-				data: Array.from(message.data)
+			let dataArray = Array.from(message.data);
+			if (dataArray.length > 10*1024) { // 10KB
+				console.log("Message over 10kb, chunking");
+				// We give it a pseudo-random ID to avoid collisions
+				const timestamp = Date.now();
+				const randomComponent = Math.random().toString(36).substring(2, 15);
+				const id = `${timestamp}-${randomComponent}`;
+				// 10KB chunks
+				let chunkSize = 10*1024;
+				let chunks = Math.floor(dataArray.length / chunkSize);
+				let lastChunkSize = dataArray.length % chunkSize;
+				let chunkCount = lastChunkSize > 0 ? chunks + 1 : chunks;
+				for (let i = 0; i < chunks; i++) {
+					let start = i * chunkSize;
+					let end = start + chunkSize;
+					let chunk = dataArray.slice(start, end);
+					payloads.push({
+						...message,
+						data: chunk,
+						chunked: {
+							index: i,
+							total: chunkCount,
+							id: id
+						}
+					});
+				}
+				if (lastChunkSize > 0) {
+					let start = chunks * chunkSize;
+					let end = start + lastChunkSize;
+					let chunk = dataArray.slice(start, end);
+					payloads.push({
+						...message,
+						data: chunk,
+						chunked: {
+							index: chunks,
+							total: chunkCount,
+							id: id
+						}
+					});
+				}
+			} else {
+				payloads.push({
+					...message,
+					data: message.data
+				});
 			}
 		} else {
-			payload = message;
+			payloads.push(message);
 		}
-		console.log("Sending", payload);
-		const encoded = encode(payload);
-		console.log("Encoded", encoded);
-		console.log("Size", encoded.byteLength / 1024)
-		const arrayBuf = this.toArrayBuffer(encoded);
-		this.channel.send(arrayBuf);
+		for (let payload of payloads) {
+			const encoded = encode(payload);
+			const arrayBuf = this.toArrayBuffer(encoded);
+			this.channel.send(arrayBuf);
+		}
 	}
 
 	toArrayBuffer(bytes) {
