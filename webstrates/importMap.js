d7d4b495d99923abbffdef5990f47f801a263564
Clemens Nylandsted Klokmose <clemens@cs.au.dk>
2024-04-18 07:56:27 +0200

    Added replace and stringify to importMap

--- webstrates/importMap.js
+++ webstrates/importMap.js
@@ -39,6 +39,26 @@ Object.defineProperty(publicObject, 'removeImport', {
 	enumerable: false
 })
 
+Object.defineProperty(publicObject, 'replace', {
+	get: () => replace,
+	set: () => { throw new Error('Internal replace method should not be modified'); },
+	// If enumerable is 'true', Puppeteer tests fail as `window.webstrate` is suddenly undefined
+	// due to the circular reference.
+	enumerable: false
+})
+
+Object.defineProperty(publicObject, 'stringify', {
+	get: () => stringify,
+	set: () => { throw new Error('Internal toString method should not be modified'); },
+	// If enumerable is 'true', Puppeteer tests fail as `window.webstrate` is suddenly undefined
+	// due to the circular reference.
+	enumerable: false
+})
+
+function stringify() {
+	return JSON.stringify(automerge.doc.meta.importMap, null, 2);
+}
+
 function remove() {
 	if (!automerge.doc.meta.importMap) return;
 	automerge.handle.change(d => delete d.meta.importMap);
@@ -57,7 +77,13 @@ function addImport(path, value) {
 function removeImport(path) {
 	if (!automerge.doc.meta.importMap || !automerge.doc.meta.importMap.imports || !automerge.doc.meta.importMap.imports[path]) return;
 	automerge.handle.change(d => delete d.meta.importMap.imports[path]);
+}
 
+function replace(importMap) {
+	if (typeof importMap === 'string') {
+		importMap = JSON.parse(importMap);
+	}
+	automerge.handle.change(d => d.meta.importMap = importMap);
 }
 
 
