ba1905dd5a85025ff53ccc4f9e26f9171298b87b
svnhub <jbk@cavi.au.dk>
2024-05-02 14:07:43 +0200

    Prepare to split vite build into two to make service-worker.js self-contained

--- vite_sw.config.ts
+++ vite_sw.config.ts
@@ -1,16 +1,17 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import { replaceCodePlugin } from "vite-plugin-replace";
 
 export default defineConfig({
     base: "./",
 
     build: {
         target: "esnext",
+        emptyOutDir: false,
         sourcemap: true,
         rollupOptions: {
             input: {
-                "main": resolve(__dirname, "index.html"),
-                "P2PSetup": resolve(__dirname, "p2p/P2PSetup.html"),
+                "service-worker": resolve(__dirname, "service-worker.js"),
             },
             output: {
                 entryFileNames: `[name].js`,
@@ -18,11 +19,24 @@ export default defineConfig({
                 assetFileNames: `[name].[ext]`,
             },
         },
-        minify: false
+        minify: false,
     },
     optimizeDeps: {
         esbuildOptions: { target: "esnext" },
     },
-    plugins: [],
+    plugins: [
+    replaceCodePlugin({
+      replacements: [
+	{ 
+	    from: "import url from \"./automerge_wasm_bg.wasm?url\";",
+	    to: "const url = location.origin+'/automerge_wasm_bg.wasm';"
+	},
+        {
+          from: "import.meta.url",
+          to: "location.origin",
+        },
+      ],
+    }),
+    ],
     define: { "process.env": {} },
 })
